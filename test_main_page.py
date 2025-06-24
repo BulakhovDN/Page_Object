33598d4d3d6e9c95757d131fc1fc6e72ffa1e465
Dmitrii <bulakhovdn@gmail.com>
2025-06-24 12:35:13 +0300

    add test_guest_should_login, test_guest_should_see_login_form, test_guest_should_see_register_form

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,6 @@
 from selenium.webdriver.common.by import By
 
+from pages.login_page import LoginPage
 from pages.main_page import MainPage
 
 
@@ -15,3 +16,24 @@ def test_guest_should_see_login_link(browser):
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+
+
+def test_guest_should_login(browser):
+    link = "https://selenium1py.pythonanywhere.com/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_url()
+
+
+def test_guest_should_see_login_form(browser):
+    link = "https://selenium1py.pythonanywhere.com/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_form()
+
+
+def test_guest_should_see_register_form(browser):
+    link = "https://selenium1py.pythonanywhere.com/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_register_form()
